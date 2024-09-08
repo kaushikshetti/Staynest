e870cea237c214b8bc91360352320acf2aa40a1d
Kaushik Shetti <kshetti29@gmail.com>
2024-09-08 23:52:39 +0530

    Added react router dom

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Header from "./components/Header";
-import NameForm from './components';
 import SearchBar from './components/SearchBar';
-import Room from './components/Room';
 import Visits from './components/Visits';
+import Room from './components/Room';
 import RentalCard from './components/Rental/RentalCard';
 import FavoritesList from './components/Rental/FavoritesList';
 const rentals = [
   { id: 1, name: 'Beach House', description: 'A nice beach house.' },
   { id: 2, name: 'Mountain Cabin', description: 'A cozy mountain cabin.' },
 ];
+
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <SearchBar/>
-     <Visits/> 
-     <Room/>
-     <div>
-      <h1>Rental Properties</h1>
-      {rentals.map(rental => (
-        <RentalCard key={rental.id} rental={rental} />
-      ))}
-      <FavoritesList />
-    </div>
-     {/* <NameForm/> */}
+      <Header />
+      <SearchBar />
+      <Visits />
+      <Room />
+      <div>
+        <h1>Rental Properties</h1>
+        {rentals.map(rental => (
+          <RentalCard key={rental.id} rental={rental} />
+        ))}
+        <FavoritesList />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,   
+
+  RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-import { FavoritesProvider } from './contexts/FavoritesContext';
+import   
+ { FavoritesProvider } from './contexts/FavoritesContext';
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App /> , 
+  },
+  {
+    path: "/home",
+    element:<Root/>
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <FavoritesProvider>
-    <App />
-  </FavoritesProvider>,
+ 
+    <FavoritesProvider>
+    <RouterProvider router={router}   
+ />
+    </FavoritesProvider>,
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results   
+ //(for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();   
